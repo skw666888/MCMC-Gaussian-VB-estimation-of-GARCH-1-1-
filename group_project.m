d2f6ef7318b76fdc3efc14b7b3d567b780756ced
xhua0336 <64999717+xhua0336@users.noreply.github.com>
2020-11-09 18:41:13 +1100

    update

--- group_project.m
+++ group_project.m
@@ -1,3 +1,11 @@
 clear all
 rng(2020) % Fix the random seed
-price = 
+price = readtable('Price')
+l = height(price) %row length of the dataframe
+%% 
+a = log(price(1,2))
+%% 
+
+for i = 1:l
+    r = log(price(i+1,2)) -log(price(i,2))
+end
